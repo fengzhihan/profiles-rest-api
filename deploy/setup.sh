8b2b6f5eb78fc62e2ae3ce84f6937f20bb3ef936
ZhihanFeng <fengzhihan98@gmail.com>
2023-07-18 23:20:08 -0700

    try: install django in virtual

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -23,6 +23,10 @@ git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
 python3 -m venv $PROJECT_BASE_PATH/env
 
+# debug
+$PROJECT_BASE_PATH/env/bin/pip install django
+# debug
+
 $PROJECT_BASE_PATH/env/bin/pip install -r
 $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
 
